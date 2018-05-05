a96b74d6f050d2b83be90e53f3c5318e1cdd3318
LucaDiVit <lucadivita.ldv@gmail.com>
2018-05-05 16:10:26 +0200

    Add files via upload

---
+++ sendAndReceiveBluetoothData.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+int rxPin = 3;
+int txPin = 2;
+SoftwareSerial bluetooth(rxPin, txPin);
+ 
+String message; //string that stores the incoming message
+ 
+void setup()
+{
+  Serial.begin(9600); //set baud rate
+  bluetooth.begin(9600); //set baud rate
+}
+ 
+void loop()
+{
+  while(bluetooth.available()){
+    message+=char(bluetooth.read());
+  }
+  if(!bluetooth.available())
+  {
+    if(message!="")
+    {//if data is available
+      Serial.println(message); //show the data
+      bluetooth.print("OK#");
+      message=""; //clear the data
+      Serial.println("OK");
+    }
+  }
+  delay(300); //delay
+}
